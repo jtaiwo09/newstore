1db63129d4114a5ef8b570b0db877e7de5a64fc1
jtaiwo09 <taiwokelvin@gmail.com>
2022-08-21 01:17:00 +0100

    commit

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -12,8 +12,6 @@ const ForgotPassword = () => {
   const [forgotPwd, { error, isError, isLoading, isSuccess }] =
     useForgotPwdMutation();
 
-  console.log(error);
-
   if (isSuccess) {
     navigate("/email-verification-sent");
   }
@@ -22,55 +20,51 @@ const ForgotPassword = () => {
     <div>
       <Navbar />
       <div className="container">
-        <div className="px-5">
-          <div className="mx-auto max-w-[380px] mt-[10%] sm:mt-[5%] animate__animated animate__fadeInUp">
-            {isError && (
-              <Alert type="error" show={isError}>
-                {error.data.message}
-              </Alert>
-            )}
+        <div className="mx-auto max-w-[380px] mt-[10%] sm:mt-[5%] animate__animated animate__fadeInUp">
+          {isError && (
+            <Alert type="error" show={isError}>
+              {error.data.message}
+            </Alert>
+          )}
+          <div className="px-5">
             <h2 className="uppercase text-center mb-2 font-[500] text-[20px] sm:text-[30px]">
               Forgot your password?
             </h2>
-            <p className="w-full mb-[24px] text-center">
+            <p className="w-full sm:mb-[24px] text-center">
               Sorry, happens to the best of us. Give us your email address and
               we’ll fix this together.
             </p>
-            <Formik
-              initialValues={{
-                email: "",
-              }}
-              validationSchema={Yup.object({
-                email: Yup.string()
-                  .email("Must be a valid email address")
-                  .required("Email is required"),
-              })}
-              onSubmit={(values, { setSubmitting }) => {
-                forgotPwd(values);
-                setSubmitting(false);
-              }}
-            >
-              <Form className="w-full border border-solid border-black/20 shadow-[0_0_20px_0_rgba(0,0,0,0.0.05)] p-[30px] bg-white rounded-[5px]">
-                <div className="mb-[20px]">
-                  <TextField
-                    name="email"
-                    type="text"
-                    placeholder="Enter email"
-                  />
-                </div>
-                <button
-                  type="submit"
-                  className="w-full upercase h-[50px] mt-[20px] p-4 rounded-[5px] text-white bg-primary flex justify-center items-center box-border"
-                >
-                  {isLoading ? (
-                    <BiLoaderCircle className="text-[30px] animate-spin duration-1000" />
-                  ) : (
-                    "Submit"
-                  )}
-                </button>
-              </Form>
-            </Formik>
           </div>
+          <Formik
+            initialValues={{
+              email: "",
+            }}
+            validationSchema={Yup.object({
+              email: Yup.string()
+                .email("Must be a valid email address")
+                .required("Email is required"),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+              forgotPwd(values);
+              setSubmitting(false);
+            }}
+          >
+            <Form className="w-full sm:border border-solid border-black/20 shadow-[0_0_20px_0_rgba(0,0,0,0.0.05)] p-[30px] bg-white rounded-[5px]">
+              <div className="mb-[20px]">
+                <TextField name="email" type="text" placeholder="Enter email" />
+              </div>
+              <button
+                type="submit"
+                className="w-full upercase h-[50px] mt-[20px] p-4 rounded-[5px] text-white bg-primary flex justify-center items-center box-border"
+              >
+                {isLoading ? (
+                  <BiLoaderCircle className="text-[30px] animate-spin duration-1000" />
+                ) : (
+                  "Submit"
+                )}
+              </button>
+            </Form>
+          </Formik>
         </div>
       </div>
     </div>
